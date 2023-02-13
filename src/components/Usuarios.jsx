e59469d00a43dbd402f977c135a0f04a16b81dd2
marquez <marquez.cmbh@gmail.com>
2023-02-13 11:19:59 -0300

    fixing errors

--- src/components/Usuarios.jsx
+++ src/components/Usuarios.jsx
@@ -6,8 +6,8 @@ export default function Usuarios(props) {
           alt="imagem de perfil" onClick={props.eventImage}/>
           <div class="texto">
             <span>
-              <strong data-test="username">{props.username}</strong>
-                <ion-icon name="pencil" onClick={props.eventUsername} data-test="name"></ion-icon>
+              <strong data-test="name">{props.username}</strong>
+                <ion-icon name="pencil" onClick={props.eventUsername} data-test="edit-name"></ion-icon>
             </span>
           </div>
         </div>
